642dea645065c0c6d41ec7dd574ca1a6741724e4
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2023-12-11 15:48:25 +0800

    feat: support to given an accessable kubeconfig (#5)
    Co-authored-by: rick <LinuxSuRen@users.noreply.github.com>

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -4,3 +4,8 @@ set -e
 mkdir -p /var/data
 
 atest run -p test-suite.yaml --report md --level debug
+
+if [[ "$K3D" == "true" ]]
+then
+    atest run -p test-suite-k3d.yaml --report md --level debug
+fi
